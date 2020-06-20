6dcab86a0512f2245d437990b69980000c69ab0d
duyminhnguyen97 <ndminh1997@gmail.com>
2020-06-20 21:16:05 +0700

    generate 16x16 block

--- data/image.py
+++ data/image.py
@@ -1,6 +1,7 @@
 import time
 import cv2
 import numpy as np
+from scipy import linalg as la
 from os import listdir, path
 from matplotlib import pyplot as plt
 # Timer
@@ -26,29 +27,55 @@ print('Weight is:',weight)
 b = []
 
 # Main
-for i in range(10):
+for i in range(1):
     # Get img RGB
     file_path = path.join(curr_path, 'images', name_list[i])
     img = cv2.imread(file_path)
     #print(img.shape)
     img = cv2.resize(img, (horizontal, vertical))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = np.asarray(img)
     img = img.astype(np.float64)
     #img = (img / 255)
-
+    print(img)
+    # plt.imshow(img, cmap="gray")
+    # plt.show()
     # Split into 3 array RGB
-    rgb = np.dsplit(img, 3)
-    green = np.asarray(rgb[1]).reshape(1024, 2048)
+    # rgb = np.dsplit(img, 3)
+    # green = np.asarray(rgb[1]).reshape(1024, 2048)
 
     temp_arr = []
 
     for j in range(16):
         for k in range(16):
-            temp_arr.append(green[j][k])
+            temp_arr.append(img[j][k])
     temp_arr = np.asarray(temp_arr).reshape(16,16)
     b.append(temp_arr)
 
 b = np.asarray(b)
 print(b)
-print(b.shape)
+#print(b.shape)
+
+# a = []
+# for i in range(1, 7):
+#     temp = []
+#     for j in range(1,7):
+#         temp.append(i)
+#     a.append(temp)
+# a = np.asarray(a)
+# print(a,'\n')
+
+rank = np.linalg.matrix_rank(b)
+
+print(rank)
+
+# (P, L, U) = la.lu(a)
+# D = np.diag(np.diag(U))
+# U /= np.diag(U)[:, None]
+
+# print(P,'\n')
+# print(L,'\n')
+# print(D,'\n')
+# print(U,'\n')
+# a_new = P.dot(L.dot(D.dot(U)))
+# print(a_new)
